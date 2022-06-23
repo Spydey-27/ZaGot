d5bac52c81dfbb2686064ec5ac3e880d03e70750
Spydey-27 <oliveira@et.esiea.fr>
2022-06-23 17:23:09 +0200

    bot discord

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,9 @@ module.exports = {
     async execute(client){
         console.log('im ready');
 
-        const devGuild = await client.guilds.cache.get('981493939154599956');
-        devGuild.commands.set(client.commands.map(cmd => cmd));
+        //const devGuild = await client.guilds.cache.get('981493939154599956');
+        //devGuild.commands.set(client.commands.map(cmd => cmd));
+        client.application.commands.set(client.commands.map(cmd => cmd));
+
     },
 };
